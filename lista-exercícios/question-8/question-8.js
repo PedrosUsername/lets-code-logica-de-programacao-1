533789dc0f19df5556dfc6d59dc3659aab180899
rogs <pilique11@gmail.com>
2022-03-01 01:21:31 -0300

    update ex08

--- lista-exercícios/question-8/question-8.js
+++ lista-exercícios/question-8/question-8.js
@@ -3,3 +3,22 @@
  * 
  * Obs: Utilize a interface html para receber os dados do usuário
  */
+let valorA = document.getElementById('valorA');
+let valorB = document.getElementById('valorB');
+let resposta = document.getElementsByTagName('span');
+
+function start() {
+    valorA.value = 1;
+    valorB.value = 1;
+}
+
+function actionEx8() {
+
+    if( Number(valorA.value) >= Number(valorB.value) ){
+        resposta[0].innerHTML = valorA.value;
+    }
+    else
+    if( Number(valorA.value) <= Number(valorB.value) ){
+        resposta[0].innerHTML = valorB.value;
+    }
+}
