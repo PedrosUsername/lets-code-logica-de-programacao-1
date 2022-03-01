8d89dd3a1d5f7cccd441e0eb6f0044f2b7b37224
rogs <pilique11@gmail.com>
2022-02-28 23:57:21 -0300

    update ex02

--- lista-exercícios/question-2/question-2.js
+++ lista-exercícios/question-2/question-2.js
@@ -3,3 +3,16 @@
  * 
  * Obs: Utilize a interface html para receber os dados do usuário
  */
+
+ let salario = document.getElementById('salario');
+ let reajuste = document.getElementById('reajuste');
+ let resposta = document.getElementsByTagName('span');
+ 
+ function start() {
+    salario.value = 1;
+    reajuste.value = 1;
+ }
+ 
+ function actionEx1() {
+    resposta[0].innerHTML = Number(salario.value) + Number(salario.value) * (Number(reajuste.value/100));
+ }
