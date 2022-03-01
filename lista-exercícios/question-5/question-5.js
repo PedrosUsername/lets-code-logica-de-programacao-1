a6e862be32ebee79b152c0f96e972b54e0204a88
rogs <pilique11@gmail.com>
2022-03-01 00:47:30 -0300

    update ex05

--- lista-exercícios/question-5/question-5.js
+++ lista-exercícios/question-5/question-5.js
@@ -2,3 +2,18 @@
  * Ler um valor e escrever a mensagem É MAIOR QUE 10! se o valor lido for maior que 10, caso contrário escrever NÃO É MAIOR QUE 10! 
  * Obs: Utilize a interface html para receber os dados do usuário
  */
+const valorB = 10;
+let valorA = document.getElementById('valorA');
+let resposta = document.getElementsByTagName('span');
+
+function start() {
+    valorA.value = 1;
+}
+
+function actionEx1() {
+
+    if( valorA.value <= valorB )
+        resposta[0].innerHTML = "NÃO";
+    else
+        resposta[0].innerHTML = "";
+}
