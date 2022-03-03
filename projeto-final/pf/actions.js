0f9b91cfa9a611b8828e349ad2208221f0dfdfb8
rogs <pilique11@gmail.com>
2022-03-02 22:47:14 -0300

    projeto final add javascript 1/2

--- projeto-final/pf/actions.js
+++ projeto-final/pf/actions.js
@@ -8,16 +8,79 @@ let b6 = document.getElementById('b6');
 let b7 = document.getElementById('b7');
 let b8 = document.getElementById('b8');
 let b9 = document.getElementById('b9');
+let bpi = document.getElementById('bpi');
+
+let bsen = document.getElementById('bsen');
+let bcon = document.getElementById('bcos');
+let bsqrt = document.getElementById('bsqrt');
+let bplu = document.getElementById('bplu');
+let bdiv = document.getElementById('bdiv');
+let bmul = document.getElementById('bmul');
+let bles = document.getElementById('bles');
+let bpow = document.getElementById('bpow');
 
 let valorA;
+let operador;
 let valorB;
 
-let resposta = document.getElementsByTagName('span');
+let visor = document.getElementsByTagName('span');
 
-function start() {
+function startAction() {
+    valorA = null;
+    operador = null;
+    valorB = null;
 
+    visor[0].innerHTML = " # ";
+    visor[1].innerHTML = " # ";
+    visor[2].innerHTML = " # ";
+    visor[4].innerHTML = " # "
 }
 
-function actionEx1() {
-    resposta[0].innerHTML = ladoA.value * ladoB.value;
+function setValue(value) {
+    if(typeof value === "string")
+        value = Math.PI;
+
+    if (valorA === null){
+        valorA = Number(value);
+        visor[0].innerHTML = value;
+    }
+    else {
+        valorB = Number(value);
+        visor[2].innerHTML = value;        
+    }
+}
+function setOperator(operator) {
+    operador = operator;
+    visor[1].innerHTML = operator;
+}
+
+function endAction() {
+    switch(operador) {
+        case '+':
+            visor[4].innerHTML = Number(valorA) + Number(valorB);
+            break;
+        case '-':
+            visor[4].innerHTML = Number(valorA) - Number(valorB);
+            break;
+        case '/':
+            visor[4].innerHTML = Number(valorA) / Number(valorB);
+            break;            
+        case '*':
+            visor[4].innerHTML = Number(valorA) * Number(valorB);
+            break;            
+        case '^':
+            visor[4].innerHTML = Number(valorA) ** Number(valorB);
+            break;            
+        case 'Sqrt()':
+            visor[4].innerHTML = Number(valorA) * Math.sqrt( Number(valorB) );
+            break;            
+        case 'Sen()':
+            visor[4].innerHTML = Number(valorA) * Math.sin(Number(valorB));
+            break;
+        case 'Cos()':
+            visor[4].innerHTML = Number(valorA) * Math.cos(Number(valorB));
+            break;
+        default:
+            alert('error');
+    } 
 }
